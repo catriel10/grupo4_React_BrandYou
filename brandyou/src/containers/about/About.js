54e0ee8bf98a88d5d3b2adaf9effca4ae12b93f3
Catriel Basile <catriel10@gmail.com>
2021-09-16 19:18:24 -0300

    CSS

--- brandyou/src/containers/about/About.js
+++ brandyou/src/containers/about/About.js
@@ -4,12 +4,23 @@ export default function About() {
     return (
         <section class="about">
             <h2>About</h2>
-            <p>THE BRAND: BrandYou is a clothing retailer company. It is part of the Spanish Inditex group (which also owns the brands of Zara, Massimo Dutti, Pull&Bear, Oysho, Uterqüe, Stradivarius and Zara Home). The company was created in April 1998 as a new store and fashion concept, aimed at a young target market.</p>
+            <h4>THE BRAND</h4>
+            <p>BrandYou is a clothing retailer company. It is part of the Spanish Inditex group (which also owns the brands of Zara, Massimo Dutti, Pull&Bear, Oysho, Uterqüe, Stradivarius and Zara Home). The company was created in April 1998 as a new store and fashion concept, aimed at a young target market.
+            In order to always have the latest trends at the shop; BrandYou uses its flexible business model to adapt to any changes occurring during the seasons, responding to them by bringing new products to the shops within the shortest possible time. The models for each season are developed entirely by their creative teams, who take as their main source of inspiration the fashion trends prevailing in the market, through information received from the shops, as well as the customers themselves. The BrandYou design team consists of more than 60 professionals who are continuously assessing the needs, desires and demands of consumers, and offering more than 4,000 different products in their shops each year. 
+            </p>
 
-            <p>THE CONCEPT
-Music, screens, projections, modern graphics,fashionable, colours...
+            <h4>THE CONCEPT</h4>
+            <p>BrandYou shops are distinguished throughout the world by their strategic location and well-detailed architecture. The establishments are converted into spacious trend-setting showcases with a carefully thought through image extending from the windows to th arrangement of the goods inside. BrandYou selects the best commercial sites in each city and positions itself in the most notable areas of the main shopping centres. Unique buildings are often chosen, which the BrandYou architectural studio refurbish and adapt to its brand image and philosophy.
+            Music, screens, projections, modern graphics, fashionable colours, contemporary furniture design, state-of-the-art lighting and so on, are all features that turn BrandYou into a shopping ‘experience’. The shop’s interior is designed to give maximum importance to displaying fashion. The layout, music, lighting, furniture, graphics and the materials used have been designed and carefully chosen to provide maximum freedom to customers as they discover the latest fashion trends. All the shop’s elements are designed by the BrandYou image team and are updated every season. BrandYou shops are designed to be appealing to young people in a space where they feel comfortable, which means the shops are fun. While browsing, choosing and trying on clothes, they can listen to the latest music and check out urban art trends displayed in the graphics and photo sessions featured in the shop decoration. BrandYou attaches great importance to its windows, displaying the most important items of the collection and the predominant theme to its public. The windows are constantly being renewed every season and the garments match the style of the shop’s interior, reflecting the trends of the whole season. The new BrandYou shop concept transforms the entire shopping space into a shop window, leaving a large glass front that allows the whole shop and complete collection to be seen.
+            </p>
+        
+               <h4>THE TARGET</h4>
+            <p>BrandYou's public is characterized by adventurous young people, who are aware of the latest trends and are interested in music, social networks and new technologies.</p>
 
-BrandYou shops are distinguished throughout the world by their strategic location and well-detailed architecture. The establishments are converted into spacious trend-setting showcases with a carefully thought through image extending from the windows to th arrangement of the goods inside. Bershka selects the best commercial sites in each city and positions itself in the most notable areas of the main shopping centres. Unique buildings are often chosen, which the Bershka architectural studio refurbish and adapt to its brand image and philosophy.</p>
+            <h4>PRODUCT LINES</h4>
+            <p>The shops have separate spaces intended for the various different product lines that BrandYou offers. In each section, BrandYou, BSK & Man, the products are placed according to their style, creating a wide spectrum ranging from casual wear to sports and from basic garments to more fashionable items.
+            The area dedicated to women's wear is the most important. This section has two product lines: BrandYou and BSK. The first focuses on the latest fashion and includes the leading trends. It has a wide range of jeans wear, eveningwear, casual wear and latest fashion-wear. BSK is the brand for younger people, and always caters to the interests and needs of this public by basing its collection on their taste in music, their idols, rock stars and so on. The men’s section offers much more than fashionable jeans wear for young people. This product line offers casual, sports and fashion wear as well. In addition, apart from the wide range of clothing, BrandYou also offers a wide range of accessories and footwear.
+            </p>        
         </section>
     )
 }
